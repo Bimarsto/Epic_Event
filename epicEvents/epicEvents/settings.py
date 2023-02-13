0659642df08657441df541bd6cc981664a2ef83b
Bimarsto <90108221+Bimarsto@users.noreply.github.com>
2023-02-13 16:44:32 +0100

    Set postgresql as default database

--- epicEvents/epicEvents/settings.py
+++ epicEvents/epicEvents/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'API',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,10 @@ WSGI_APPLICATION = 'epicEvents.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'epic_events_crm',
+        'USER': 'epic_events_admin',
+        'PASSWORD': 'admin',
     }
 }
 
