077ce38e38f2acf679ae7f21e3877b2a1dd0c364
Bimarsto <90108221+Bimarsto@users.noreply.github.com>
2023-02-21 14:23:51 +0100

    Add views and urls core

--- epicEvents/API/views.py
+++ epicEvents/API/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Client, Contract, Event
+from .serializers import ClientListSerializer, ClientDetailSerializer, \
+                         ContractListSerializer, ContractDetailSerializer, \
+                         EventListSerializer, EventDetailSerializer
+
+
+class ClientViewSet(ModelViewSet):
+
+    serializer_class = ClientListSerializer
+    detail_serializer_class = ClientDetailSerializer
+
+    def get_queryset(self):
+        return Client.objects.all
+
+
+class ContractViewSet(ModelViewSet):
+
+    serializer_class = ContractListSerializer
+    detail_serializer_class = ContractDetailSerializer
+
+    def get_queryset(self):
+        return Contract.objects.all
+
+
+class EventViewSet(ModelViewSet):
+
+    serializer_class = EventListSerializer
+    detail_serializer_class = EventDetailSerializer
+
+    def get_queryset(self):
+        return Event.objects.all
