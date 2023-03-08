d96757796a117eaa692044aa555d4449661199c6
Bimarsto <90108221+Bimarsto@users.noreply.github.com>
2023-03-08 12:02:39 +0100

    Add sentry logs

--- epicEvents/epicEvents/urls.py
+++ epicEvents/epicEvents/urls.py
@@ -5,6 +5,13 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from API.views import ClientViewSet, ContractViewSet, EventViewSet, UserViewSet
 
+from django.urls import path
+
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+
 routers = routers.SimpleRouter()
 routers.register('clients', ClientViewSet, basename='clients')
 routers.register('contracts', ContractViewSet, basename='contracts')
@@ -17,5 +24,6 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/', include(routers.urls))
+    path('api/', include(routers.urls)),
+    path('sentry-debug/', trigger_error),
     ]
